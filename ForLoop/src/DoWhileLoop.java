5c2426e8fc693964ecae20758112f04f349b2387
Parikshith M L <107392773+Parikshithgowda@users.noreply.github.com>
2023-08-10 21:14:11 +0530

    Delete ForLoop directory

--- ForLoop/src/DoWhileLoop.java
+++
@@ -1,9 +0,0 @@
-public class DoWhileLoop {
-    public static void main(String[] args) {
-        int i =1201;
-        do{
-            System.out.println("Sigmaaaa");
-            i++;
-        }while(i<=1210);
-    }
-}
--- ForLoop/src/WhileLoop.java
+++
@@ -1,9 +0,0 @@
-public class WhileLoop {
-    public static void main(String[] args) {
-        int i= 600;
-        while (i<=609) {
-            System.out.println("Hello King");
-            i++;
-        }
-    }
-}
